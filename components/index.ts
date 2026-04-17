42df208812e9c5713380b029aa33bbdd1eae0bbf
Vishva Patel <vishva.viking@gmail.com>
2026-04-18 01:58:51 +0530

    fixed export error

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
 export { Hero } from './Hero';
 export { Projects } from './Projects';
-export { USMHACSPanel } from './USMHACSPanel';
+export { default as USMHACSPanel } from './USMHACSPanel';
 export { default as SecureCommunication } from './SecureCommunication';
